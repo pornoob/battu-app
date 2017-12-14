de4f388fb6d6f042e963f03b63a6be98c0278dfb
matiasmenares <dejaaymts@gmail.com>
2017-12-14 18:00:02 -0300

    Set Pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,16 +11,28 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+// Agregadas por apptec
+import { WelcomePage } from '../pages/welcome/welcome';
+import { LoginPage } from '../pages/login/login';
+import { SignupPage } from '../pages/signup/signup';
+
+import { HttpModule } from '@angular/http';
+import { AuthService } from '../providers/auth-service/auth-service';
+
+
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
     ContactPage,
     HomePage,
+    WelcomePage,
+    LoginPage,
+    SignupPage,
     TabsPage
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -29,12 +41,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    WelcomePage,
+    LoginPage,
+    SignupPage,
     TabsPage
   ],
   providers: [
     StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    SplashScreen, AuthService,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
   ]
 })
 export class AppModule {}
